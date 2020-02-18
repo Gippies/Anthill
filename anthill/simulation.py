58b2a36e0645b03462d5070be86c3738e38c265e
Joshua Patterson <jleegippies@gmail.com>
2020-02-17 18:17:28 -0600

    Added an underground view

--- anthill/simulation.py
+++ anthill/simulation.py
@@ -2,18 +2,28 @@ from anthill.groups import Colony, PlantKingdom
 from anthill.utils.graphics import GraphicView
 
 
+class View:
+    OUTSIDE = 1
+    UNDERGROUND = 2
+
+
 class Simulation:
     def __init__(self):
         self.colony = Colony()
         self.plant_kingdom = PlantKingdom()
+        self.view = View.OUTSIDE
 
         graphic_components = [self.colony.hill]
         graphic_components.extend(self.colony.ants)
         graphic_components.extend(self.plant_kingdom.leafies)
         self.outside_view = GraphicView(graphic_components)
+        self.underground_view = GraphicView([])
 
     def draw(self):
-        self.outside_view.draw()
+        if self.view == View.OUTSIDE:
+            self.outside_view.draw()
+        elif self.view == View.UNDERGROUND:
+            self.underground_view.draw()
 
     def update(self, delta_time):
         self.colony.update(self.plant_kingdom.leafies, delta_time)
--- runner.py
+++ runner.py
@@ -1,6 +1,7 @@
 import pyglet
+from pyglet.window import mouse
 
-from anthill.simulation import Simulation
+from anthill.simulation import Simulation, View
 from settings import SCREEN_WIDTH, SCREEN_HEIGHT, FRAME_RATE
 
 window = pyglet.window.Window(SCREEN_WIDTH, SCREEN_HEIGHT)
@@ -11,6 +12,14 @@ simulation = Simulation()
 def on_draw():
     window.clear()
     simulation.draw()
+
+
+@window.event
+def on_mouse_press(x, y, button, modifiers):
+    if button == mouse.LEFT:
+        simulation.view = View.UNDERGROUND
+    elif button == mouse.RIGHT:
+        simulation.view = View.OUTSIDE
 
 
 pyglet.clock.schedule_interval(simulation.update, 1.0 / FRAME_RATE)
